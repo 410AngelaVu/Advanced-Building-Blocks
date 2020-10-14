2d9a1b34676e4449dd1502228e13194e940ac9b0
Ramon-Carrillo <carrillojrramon>
2020-10-14 13:23:57 -0500

    adjusted my_each to print

--- advanced_building_blocks.rb
+++ advanced_building_blocks.rb
@@ -1,10 +1,10 @@
 module Enumerable
   def my_each
-  i = 0
-  while i < array.length
-  yield array[i]
-  i += 1
-  end
-  array
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+    array
   end
 end
